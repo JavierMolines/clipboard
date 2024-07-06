d7c551d8e51b01e4fca559e2e0afc78432930c38
JavierMolines <eljavimolines@gmail.com>
2024-07-06 08:07:43 -0300

    Feature: load navbar and create

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,14 +1,23 @@
 import type { Routes } from "@angular/router";
+import NavbarComponent from "@components/navbar/navbar.component";
+import CreateComponent from "@pages/create/create.component";
+import ListComponent from "@pages/list/list.component";
 
 export const routes: Routes = [
 	{
 		path: "",
-		loadComponent: () => import("@components/navbar/navbar.component"),
+		//loadComponent: () => import("@components/navbar/navbar.component"),
+		component: NavbarComponent,
 		children: [
-			{ path: "", loadComponent: () => import("@pages/list/list.component") },
+			{
+				path: "",
+				//loadComponent: () => import("@pages/list/list.component") },
+				component: ListComponent,
+			},
 			{
 				path: "create",
-				loadComponent: () => import("@pages/create/create.component"),
+				//loadComponent: () => import("@pages/create/create.component"),
+				component: CreateComponent,
 			},
 			{
 				path: "settings",
