ff26905a44a6977d815cf7527ada46a9f29a45d3
JavierMolines <eljavimolines@gmail.com>
2024-12-27 07:20:47 -0300

    feat: chuck lazy AND title and description by page

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,7 @@
-import { Component } from "@angular/core";
+import { Component, OnInit } from "@angular/core";
+import { Title } from "@angular/platform-browser";
+import { Meta } from "@angular/platform-browser";
+import { ActivatedRoute, NavigationEnd, Router } from "@angular/router";
 import { RouterModule } from "@angular/router";
 
 @Component({
@@ -6,6 +9,35 @@ import { RouterModule } from "@angular/router";
 	imports: [RouterModule],
 	templateUrl: "./app.component.html",
 })
-export class AppComponent {
-	title = "clipboard";
+export class AppComponent implements OnInit {
+	constructor(
+		private router: Router,
+		private activatedRoute: ActivatedRoute,
+		private titleService: Title,
+		private metaService: Meta,
+	) {}
+
+	ngOnInit() {
+		// biome-ignore lint/complexity/noForEach: <explanation>
+		const events = this.router.events.forEach((event) => {
+			if (event instanceof NavigationEnd) {
+				let currentRoute = this.activatedRoute;
+
+				while (currentRoute.firstChild) {
+					currentRoute = currentRoute.firstChild;
+				}
+
+				const routeData = currentRoute.snapshot.data;
+				if (routeData["title"]) {
+					this.titleService.setTitle(routeData["title"]);
+				}
+				if (routeData["description"]) {
+					this.metaService.updateTag({
+						name: "description",
+						content: routeData["description"],
+					});
+				}
+			}
+		});
+	}
 }
