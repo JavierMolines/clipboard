d4c389dea95bcd699b60b459e8d95a12104af32c
JavierMolines <eljavimolines@gmail.com>
2026-02-15 09:20:09 -0300

    fix(dom): remove error logging from focusDomById function

--- src/app/utils/dom.ts
+++ src/app/utils/dom.ts
@@ -2,7 +2,5 @@ export const focusDomById = (id: string) => {
 	try {
 		const areaTextInput = document.getElementById(id) as HTMLElement;
 		areaTextInput.focus();
-	} catch (error) {
-		console.error(`Error focusing element with id ${id}:`, error);
-	}
+	} catch {}
 };
